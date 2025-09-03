10978df9f96732ab9f2eb09e1b161dd6a228bb55
erjbroek <broe0136@hz.nl>
2025-09-03 21:14:46 +0200

    first commit

---
+++ super-mario-world-usa/main.py
@@ -0,0 +1,17 @@
+import retro
+
+
+def main():  
+  env = retro.make(game='SuperMarioWorld-Snes')
+  env.reset()
+  while True:
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    env.render()
+    if info["lives"] == 3:
+      env.reset()
+
+  env.close()
+
+if __name__ == "__main__":
+  main()
