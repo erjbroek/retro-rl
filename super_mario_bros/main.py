e8d2196be6f64875b0491eabb0b02332f769c7fc
erjbroek <broe0136@hz.nl>
2025-09-12 12:47:06 +0200

    added framestacking

--- super_mario_bros/main.py
+++ super_mario_bros/main.py
@@ -1,30 +1,30 @@
 import retro
 from gymnasium.wrappers import GrayscaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-import matplotlib.pyplot as plt
-from gym.spaces import Box
 
 class MarioAI:
   def __init__(self):
     self.env = retro.make(
       game="SuperMarioBros-Nes",
       scenario="./scenario.json",
-      state="./Level1-1.state"
+      state="./Level8-1.state"
     )
     
     self.preprocess()
 
   def preprocess(self):
-    self.env = GrayscaleObservation(self.env)
+    self.env = GrayscaleObservation(self.env, keep_dim=True)
+    self.env = DummyVecEnv([lambda: self.env])
+    self.env = VecFrameStack(self.env, 4, channels_order='last')
 
   def run(self):
     self.env.reset()
     while True:
       action = self.env.action_space.sample()
-      observation, reward, terminated, truncated, info = self.env.step(action)
+      obs, reward, done, info = self.env.step([action])
 
       self.env.render()
-      if terminated or truncated:
+      if done:
         self.env.reset()
 
 
