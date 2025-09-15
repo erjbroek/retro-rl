dc1d194f9398090f1363e3a542c01ff7151cbcd2
erjbroek <broe0136@hz.nl>
2025-09-15 15:18:32 +0200

    fixed model initialisation after first training round

--- super_mario_bros/main.py
+++ super_mario_bros/main.py
@@ -3,12 +3,11 @@ from gymnasium.wrappers import GrayscaleObservation
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
 from stable_baselines3 import PPO
 from train_logging_callback import TrainAndLoggingCallback
-from wrappers import ResetEnvWrapper, RewardPrinterWrapper
 
 CHECKPOINT_DIRECTORY = './train/'
 LOG_DIRECTORY = './logs/'
 callback = TrainAndLoggingCallback(check_freq=200000, save_path=CHECKPOINT_DIRECTORY)
-total_steps = 20000
+total_steps = 2000
 check_count = 4
 
 class MarioAI:
@@ -25,12 +24,14 @@ class MarioAI:
                      learning_rate=0.0001, n_steps=512)
     self.loaded = False
 
-  def reset(self, render_mode):
+  def reset(self, render_mode, should_record):
+    record_dir = '.' if should_record else None
     self.env = retro.make(
       game="SuperMarioBros-Nes",
       scenario="./scenario.json",
       state="./Level1-1.state",
-      render_mode=render_mode
+      render_mode=render_mode,
+      record=record_dir
     )
     self.preprocess()
   
@@ -42,22 +43,23 @@ class MarioAI:
   def train(self):
     if (self.loaded):
       self.model = PPO.load('testmodel')
+      self.model.set_env(self.env)
     self.model.learn(total_timesteps=total_steps / check_count, callback=callback)
     self.model.save('testmodel')
     self.loaded = True
 
   def run(self):
     self.env.close()
-    self.reset(render_mode='human')
+    self.reset(render_mode='human', should_record=True)
     state = self.env.reset()
-    done = False
-    while not done:
+    finished = False
+    while not finished:
       action, _ = self.model.predict(state)
       obs, reward, done, info = self.env.step(action)
-
-      if done:
+      if info[0]['time'] <= 300:
+        finished = True
         self.env.close()
-        self.reset(render_mode=None)
+        self.reset(render_mode=None, should_record=False)
 
 if __name__ == "__main__":
   mario_ai = MarioAI()
