806ac5deba794996300b33f25d1db7311399c2a5
erjbroek <broe0136@hz.nl>
2025-09-15 15:41:28 +0200

    added comments to methods

--- super_mario_bros/main.py
+++ super_mario_bros/main.py
@@ -24,6 +24,8 @@ class MarioAI:
                      learning_rate=0.0001, n_steps=512)
     self.loaded = False
 
+  # because the environments render mode cannot be changed outside of initialisation,
+  # this reset function resets the environment so that the render_mode can be changed
   def reset(self, render_mode, should_record):
     record_dir = '.' if should_record else None
     self.env = retro.make(
@@ -35,19 +37,40 @@ class MarioAI:
     )
     self.preprocess()
   
+  # preprocesses the environment.
+  # - GrayscaleObservation turns the rgb environment gray scaled:
+  #     This reduces the input dimensionality (from 3 color channels to 1), which speeds up training and 
+  #     reduces memory usage. Most tasks don’t need color information, so this is often sufficient. An
+  #     visualisation of this is found in the assets directory state.png.
+  #
+  # - DummyVecEnv wraps the environment for vectorized operations:
+  #     Many RL libraries expect environments to support batch processing (multiple environments at once),
+  #     even if you only use one. DummyVecEnv provides this interface for a single environment,
+  #     which makes sure it's compatible
+  # .
+  # - VecFrameStack stacks 4 consecutive frames for temporal information:
+  #     Stacks several consecutive frames (In this case 4) together, helping it 'see' motion (since a 
+  #     single frame doesn’t show movement). Example, in games, knowing the last few frames helps the
+  #     agent understand object velocities and directions. Example of this is found in the assets 
+  #     directory observation3.png, showing the 4 consecutive frames.
   def preprocess(self):
     self.env = GrayscaleObservation(self.env, keep_dim=True)
     self.env = DummyVecEnv([lambda: self.env])
     self.env = VecFrameStack(self.env, 4, channels_order='last')
 
+  # This is what actually loads and trains the model
+  #   Here, the training is split up into check_count number of ste.ps, which enables us to visualise
+  #   the agent acting in the environment every once in a while
   def train(self):
     if (self.loaded):
-      self.model = PPO.load('testmodel')
+      self.model = PPO.load('SMB_PPO')
       self.model.set_env(self.env)
     self.model.learn(total_timesteps=total_steps / check_count, callback=callback)
-    self.model.save('testmodel')
+    self.model.save('SMB_PPO')
     self.loaded = True
 
+  # Visualises the agent in the environment. Important to mention this is seperate for training the
+  # agent, and is just run to see mario playing the game.
   def run(self):
     self.env.close()
     self.reset(render_mode='human', should_record=True)
